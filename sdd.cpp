c300d800ef5d6bb704078e95837c3034d9aeb740
naoki kageyama <naoki.kp@gmail.com>
2013-10-20 20:38:43 +0900

    indent fixed

--- sdd.cpp
+++ sdd.cpp
@@ -1,16 +1,16 @@
-//	sdd.cpp
+//  sdd.cpp
 
 #include <windows.h>
 #include <tchar.h>
 
 HHOOK hHook;
 LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam){
-	if(nCode >= 0){
-		if(nCode == HCBT_ACTIVATE){
-			SetDlgItemText((HWND)wParam, IDOK, _T("êiíªÇæÇﬂÇ≈Ç∑"));
-		}
-	}
-	return CallNextHookEx(hHook, nCode, wParam, lParam);
+    if(nCode >= 0){
+        if(nCode == HCBT_ACTIVATE){
+            SetDlgItemText((HWND)wParam, IDOK, _T("êiíªÇæÇﬂÇ≈Ç∑"));
+        }
+    }
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hCurInst, HINSTANCE hPrevInst, LPSTR lpsCmdLine, int nCmdShow){
